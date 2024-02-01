102108ad25a0172b06a69cd78c9829852f23d9b6
Ilya Bezrukov <bezrukoff888@gmail.com>
2024-02-02 01:44:49 +0300

    Add better config

--- main.py
+++ main.py
@@ -1,19 +1,32 @@
+from dataclasses import dataclass
+
 from vk_api.vk_api import VkApi
 from vk_api.bot_longpoll import *
 
-GROUP_ID = 176036515
-TEXT = "Это автоматический комментарий от сообщества!"
+from config import ConfigClass, load_config
 
-with open("token.txt") as f:
-    TOKEN = f.readline().strip()
 
-vk = VkApi(token=TOKEN)
-bot = VkBotLongPoll(vk, GROUP_ID)
+@dataclass
+class Config (ConfigClass):
+    token: str
+    group_id: int
+    messages: list[str]
+
+    def __post_init__(self):
+        if not self.token:
+            raise RuntimeError("No token is provided!")
+
+
+config = load_config(Config)
+
+vk = VkApi(token=config.token)
+bot = VkBotLongPoll(vk, config.group_id)
 
 for event in bot.listen():
     if event.type == VkBotEventType.WALL_POST_NEW:
-        vk.method("wall.createComment", {
-            "owner_id": event.obj.get("owner_id"),
-            "post_id": event.obj.get("id"),
-            "message": TEXT,
-        })
+        for text in config.messages:
+            vk.method("wall.createComment", {
+                "owner_id": event.obj.get("owner_id"),
+                "post_id": event.obj.get("id"),
+                "message": text,
+            })
